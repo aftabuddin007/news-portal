6695de97919273b70a56a03802df49c2af2e1d32
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-08-28 20:00:40 +0600

    fix navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 import { format } from 'date-fns';
 const Header = () => {
     return (
-        <div className='flex justify-center flex-col items-center gap-3'>
+        <div className='flex justify-center flex-col items-center mt-10 gap-3'>
             <img className='w-[380px]' src={logo} alt="" />
             <p className='text-accent'>Journalism Without Fear or Favour</p>
             <p className='text-semibold text-accent'>{format (new Date(),'EEEE, MMMM MM, yyyy')}</p>
